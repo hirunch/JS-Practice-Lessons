82fd3ca28a996c586a31693e8ddb0666bdfa28f8
Hiruna Chami <116061206+hirunch@users.noreply.github.com>
2025-02-17 20:04:01 +0530

    updates

--- Lesson 7/getter_setter.js
+++ Lesson 7/getter_setter.js
@@ -1,1 +1,21 @@
 //getter and setter in javascript
+
+class Dog{
+  constructor(name, breed){
+    this.name = name;
+    this.breed = breed;
+    this._age = 0;
+  }
+
+  get age(){
+    return this._age; 
+  }
+
+  set age(value){
+    this._age = value;
+  }
+}
+
+const dogOne = new Dog();
+dogOne.age = 5;
+console.log(dogOne.age);
